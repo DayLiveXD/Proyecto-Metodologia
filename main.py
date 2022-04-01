99abe3bb2eba71d61341af07b5a49f4585626785
DayLiveXD <89877107+DayLiveXD@users.noreply.github.com>
2022-04-01 18:32:09 -0300

    Se agregaron mas winning moves y se arreglo la identacion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pygame
 
 ROWS = 6
 COLS = 7
@@ -12,16 +13,13 @@ def create_board():
 def drop_piece(board, row, col, piece):
     board[row][col] = piece
 
-
 def is_valid_location(board, col):
     return board[ROWS-1][col] == 0
-
 
 def get_next_open_row(board, col):
     for r in range(ROWS):
         if board[r][col] == 0:
             return r
-
 
 def print_board(board):
     print(np.flip(board, 0))
@@ -33,10 +31,20 @@ def winning_move(board, piece):
               if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
                   return True
      #Verificar movimientos ganadores verticales
-     for c in range(COLS):
-         for r in range(ROWS - 3):
-             if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
-                 return True
+      for c in range(COLS):
+          for r in range(ROWS - 3):
+              if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                  return True
+     #Verificar movimientos ganadores positivos diagonales
+      for c in range(COLS - 3):
+                for r in range(ROWS - 3):
+                    if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                        return True
+      #Verificar movimientos ganadores negativos diagonales
+      for c in range(COLS - 3):
+                for r in range(3, ROWS):
+                    if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                        return True
 
 ####################################################################################################################################
 
